2f1e26a4b813ed194fafcea13910710dd1c10014
Ivan <bondar@quality-lab.ru>
2017-01-19 15:50:07 +0200

    Изменение форматирования строки

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
- public static void main(String[] args) { System.out.println("Hello, world!");  }
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
