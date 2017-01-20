0488bf0477e89b88ae2b4cbec2a7e88f986207e3
Ivan <bondar@quality-lab.ru>
2017-01-20 22:23:30 +0200

    Применение метода вместо  функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,26 +3,20 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("word");
-    hello("user");
-    hello("Ivan");
+    hello ( "word" );
+    hello ( "user" );
+    hello ( "Ivan" );
 
-    Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    Square s = new Square ( 5 );
+    System.out.println ( "Площадь квадрата со стороной " + s.l + " = " + s.area () );
 
-    Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    Rectangle r = new Rectangle ( 4, 6 );
+    System.out.println ( "Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area () );
   }
 
   public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
+    System.out.println ( "Hello, " + somebody + "!" );
   }
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
 
-  public static double area(Rectangle r) {
-    return r.a * r.b;
-  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -11,6 +11,11 @@ public class Rectangle {
     this.a = a;
     this.b = b;
 
-
   }
+
+  public double area() {
+    return this.a * this.b;
+  }
+
+
 }
