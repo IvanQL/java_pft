9547d2ea6123e21c75d6559640ba9b88bfbe620a
Ivan <bondar@quality-lab.ru>
2017-01-20 17:09:49 +0200

    Добавлены объекты и конструкторы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Administrator on 20.01.2017.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+
+
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Administrator on 20.01.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
