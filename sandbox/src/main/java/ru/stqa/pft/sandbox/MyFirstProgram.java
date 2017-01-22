0f0e87f94e13652616ea6b10f1e47262788279b4
Ivan <bondar@quality-lab.ru>
2017-01-22 17:00:23 +0200

    Добавлен конструктор в объект Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,13 +13,9 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle ( 4, 6 );
     System.out.println ( "Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area () );
 
-    Point p1 = new Point();
-    Point p2 = new Point();
+    Point p1 = new Point(4, 4);
+    Point p2 = new Point(8, 10);
 
-    p1.x = 4;
-    p1.y = 4;
-    p2.x = 8;
-    p2.y = 10;
     System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y +") и p2(" + p2.x + ";" + p2.y + ") = " + distance(p1,p2));
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,4 +7,11 @@ public class Point {
   public double x;
   public double y;
 
+
+  public Point(double x, double y) {
+
+    this.x =x;
+    this.y =y;
+  }
+
   }
