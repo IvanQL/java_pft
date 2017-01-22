75ccdeca5894df8957eb6896b147a6bdb96578bb
Ivan <bondar@quality-lab.ru>
2017-01-22 16:53:34 +0200

    Добавлен объект Point и функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,11 +12,27 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle ( 4, 6 );
     System.out.println ( "Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area () );
+
+    Point p1 = new Point();
+    Point p2 = new Point();
+
+    p1.x = 4;
+    p1.y = 4;
+    p2.x = 8;
+    p2.y = 10;
+    System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y +") и p2(" + p2.x + ";" + p2.y + ") = " + distance(p1,p2));
+
+
   }
+
+
 
   public static void hello(String somebody) {
     System.out.println ( "Hello, " + somebody + "!" );
   }
 
 
+    public static double distance (Point p1, Point p2) {
+  return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Administrator on 20.01.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  }
