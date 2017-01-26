35e40e56771560f4c21e1e0ccdf3d57312d9c661
Ivan <bondar@quality-lab.ru>
2017-01-26 13:49:29 +0200

    Методы сделаны параметризированными. Добавлен класс ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -4,18 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class ContactCreationTests {
   FirefoxDriver wd;
@@ -25,16 +17,16 @@ public class ContactCreationTests {
     wd = new FirefoxDriver ();
     wd.manage ().timeouts ().implicitlyWait ( 60, TimeUnit.SECONDS );
     wd.get ( "http://localhost/addressbook/" );
-    login ();
+    login ( "admin", "secret" );
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement ( By.name ( "user" ) ).click ();
     wd.findElement ( By.name ( "user" ) ).clear ();
-    wd.findElement ( By.name ( "user" ) ).sendKeys ( "admin" );
+    wd.findElement ( By.name ( "user" ) ).sendKeys ( username );
     wd.findElement ( By.name ( "pass" ) ).click ();
     wd.findElement ( By.name ( "pass" ) ).clear ();
-    wd.findElement ( By.name ( "pass" ) ).sendKeys ( "secret" );
+    wd.findElement ( By.name ( "pass" ) ).sendKeys ( password );
     wd.findElement ( By.xpath ( "//form[@id='LoginForm']/input[3]" ) ).click ();
   }
 
@@ -42,7 +34,7 @@ public class ContactCreationTests {
   public void testContactCreation() {
 
     gotoAddContactPage ();
-    fillContactForm ();
+    fillContactForm ( new ContactData ( "ivan", "bondar", "0981234567", "test@mail.com" ) );
     enterNewContact ();
   }
 
@@ -50,19 +42,19 @@ public class ContactCreationTests {
     wd.findElement ( By.xpath ( "//div[@id='content']/form/input[21]" ) ).click ();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(ContactData contactData) {
     wd.findElement ( By.name ( "firstname" ) ).click ();
     wd.findElement ( By.name ( "firstname" ) ).clear ();
-    wd.findElement ( By.name ( "firstname" ) ).sendKeys ( "ivan" );
+    wd.findElement ( By.name ( "firstname" ) ).sendKeys ( contactData.getName () );
     wd.findElement ( By.name ( "lastname" ) ).click ();
     wd.findElement ( By.name ( "lastname" ) ).clear ();
-    wd.findElement ( By.name ( "lastname" ) ).sendKeys ( "bondar" );
+    wd.findElement ( By.name ( "lastname" ) ).sendKeys ( contactData.getLastname () );
     wd.findElement ( By.name ( "home" ) ).click ();
     wd.findElement ( By.name ( "home" ) ).clear ();
-    wd.findElement ( By.name ( "home" ) ).sendKeys ( "0981234567" );
+    wd.findElement ( By.name ( "home" ) ).sendKeys ( contactData.getTelephone () );
     wd.findElement ( By.name ( "email" ) ).click ();
     wd.findElement ( By.name ( "email" ) ).clear ();
-    wd.findElement ( By.name ( "email" ) ).sendKeys ( "test@mail.com" );
+    wd.findElement ( By.name ( "email" ) ).sendKeys ( contactData.getEmail () );
   }
 
   private void gotoAddContactPage() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String lastname;
+  private final String telephone;
+  private final String email;
+
+  public ContactData(String name, String lastname, String telephone, String email) {
+    this.name = name;
+    this.lastname = lastname;
+    this.telephone = telephone;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getTelephone() {
+    return telephone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
