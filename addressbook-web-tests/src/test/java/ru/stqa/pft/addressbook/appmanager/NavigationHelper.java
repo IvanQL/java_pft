cb668b261bf717c950091f5da845bed900f25413
Ivan <bondar@quality-lab.ru>
2017-02-02 16:36:52 +0200

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,7 +14,15 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent ( By.tagName ( "h1" ) )
+            && wd.findElement ( By.tagName ( "h1" ) ).getText ().equals ( "Groups" )
+            && isElementPresent ( By.name ( "new" ) )) {
+      return;
+
+    }
     click ( By.linkText ( "groups" ) );
+
+
   }
 
   public void gotoAddContactPage() {
@@ -22,6 +30,9 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoHomePage() {
+    if (isElementPresent ( By.id ( "maintable" ) )) {
+      return;
+    }
     click ( By.linkText ( "home" ) );
   }
 }
