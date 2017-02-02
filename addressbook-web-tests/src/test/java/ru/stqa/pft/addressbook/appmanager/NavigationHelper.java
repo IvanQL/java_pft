abc7ddcf9c2be6eef9eed6ae705f0874ea6196b6
Ivan <bondar@quality-lab.ru>
2017-02-02 16:54:34 +0200

    Оптимизирован переход между страницами для gotoAddContactPage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -26,6 +26,12 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoAddContactPage() {
+
+    if (isElementPresent (  By.tagName ( "h1" ))
+      && wd.findElement ( By.tagName ( "h1" ) ).getText ().equals ( "Edit / add address book entry" )
+      && isElementPresent ( By.name ( "submit" ) ) ) {
+      return;
+    }
     click ( By.linkText ( "add new" ) );
   }
 
