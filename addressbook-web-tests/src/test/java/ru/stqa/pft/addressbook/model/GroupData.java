6c717c02fce062225686f9c0984dceea9f97bc57
Ivan <bondar@quality-lab.ru>
2017-02-09 18:45:44 +0200

    Добавлена проверка сравнения списка целиком для удаления группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -15,11 +15,35 @@ public class GroupData {
     return name;
   }
 
+
+
   public String getHeader() {
     return header;
   }
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass () != o.getClass ()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals ( groupData.name ) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode () : 0;
   }
 }
