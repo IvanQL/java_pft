9c0bbef9efc0720e69f49f330909dc75c02a3610
Ivan <bondar@quality-lab.ru>
2017-02-09 17:57:31 +0200

    Реализовано сравнение длины списка для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 28.01.2017.
  */
@@ -13,17 +15,17 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
 
     app.getNavigationHelper ().gotoGroupPage ();
-    int before = app.getGroupHelper ().getGroupCount();
     if (!app.getGroupHelper ().isThereAGroup ()) {
       app.getGroupHelper ().createGroup ( new GroupData ( "test1", null, null ) );
     }
-    app.getGroupHelper ().selectGroup (before - 1);
+    List<GroupData> before = app.getGroupHelper ().getGroupList();
+    app.getGroupHelper ().selectGroup (before.size () - 1);
     app.getGroupHelper ().initGroupModification ();
     app.getGroupHelper ().fillGroupForm ( new GroupData ( "test1", "test2", "test3" ) );
     app.getGroupHelper ().submitGroupModification ();
     app.getGroupHelper ().returnToGroupPage ();
-    int after = app.getGroupHelper ().getGroupCount();
-    Assert.assertEquals (after, before );
+    List<GroupData> after = app.getGroupHelper ().getGroupList();
+    Assert.assertEquals (after.size (), before.size () );
 
   }
 
