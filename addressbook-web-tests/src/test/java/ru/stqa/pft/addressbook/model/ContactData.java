9b707cda2ae9126f6fed177ee8b44afd97adb63d
Ivan <bondar@quality-lab.ru>
2017-02-09 19:46:23 +0200

    Добавлена проверка сравнения списка целиком для удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -34,4 +34,30 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "name='" + name + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass () != o.getClass ()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (name != null ? !name.equals ( that.name ) : that.name != null) return false;
+    return lastname != null ? lastname.equals ( that.lastname ) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name != null ? name.hashCode () : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode () : 0);
+    return result;
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -36,5 +36,8 @@ public class ContactDeletionTests extends TestBase {
     List<ContactData> after = app.getContactHelper ().getContactList();
     Assert.assertEquals (after.size (), before.size () - 1);
 
+    before.remove ( before.size () -1 );
+    Assert.assertEquals(before, after);
+
   }
 }
