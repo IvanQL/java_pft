5a8ee2610874082d491fe446899bec6e6d0a2c9d
Ivan <bondar@quality-lab.ru>
2017-02-13 22:01:08 +0200

    Реализована лямбда при создании контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,20 +21,14 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper ().gotoHomePage ();
     List<ContactData> before = app.getContactHelper ().getContactList();
     app.getNavigationHelper ().gotoAddContactPage ();
-    ContactData contact = new ContactData ("ivan", "bondar", "0981234567", "test@mail.com", "test1"  );
+    ContactData contact = new ContactData ("ivan1", "bondar", "0981234567", "test@mail.com", "test1"  );
     app.getContactHelper ().createContact ( contact, true );
     app.getNavigationHelper ().gotoHomePage ();
     List<ContactData> after = app.getContactHelper ().getContactList();
     Assert.assertEquals (after.size (), before.size () + 1);
 
 
-    int max = 0;
-    for (ContactData g : after) {
-      if (g.getId () > max) {
-        max = g.getId ();
-      }
-    }
-    contact.setId ( max );
+    contact.setId ( after.stream ().max ( Comparator.comparingInt ( ContactData::getId ) ).get ().getId () );
     before.add ( contact );
     Assert.assertEquals ( new HashSet<Object> ( before ), new HashSet <Object> ( after ) );
 
