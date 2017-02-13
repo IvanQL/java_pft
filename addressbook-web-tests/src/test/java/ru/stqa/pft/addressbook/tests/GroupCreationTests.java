0cf28bb3015623255d3214547211c32010f1bf37
Ivan <bondar@quality-lab.ru>
2017-02-13 21:57:51 +0200

    Реализована лямбда при создании группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,19 +16,12 @@ public class GroupCreationTests extends TestBase {
 
     app.getNavigationHelper ().gotoGroupPage ();
     List <GroupData> before = app.getGroupHelper ().getGroupList ();
-    GroupData group = new GroupData ( "test1", null, null );
+    GroupData group = new GroupData ( "test3", null, null );
     app.getGroupHelper ().createGroup ( group );
     List <GroupData> after = app.getGroupHelper ().getGroupList ();
     Assert.assertEquals ( after.size (), before.size () + 1 );
 
-
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId () > max) {
-        max = g.getId ();
-      }
-    }
-    group.setId ( max );
+    group.setId ( after.stream ().max ( Comparator.comparingInt ( GroupData::getId ) ).get ().getId () );
     before.add ( group );
     Assert.assertEquals ( new HashSet <Object> ( before ), new HashSet <Object> ( after ) );
 
