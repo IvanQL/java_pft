9c71ad898e104f9a50fd21dcb7644930593dd4a0
Ivan <bondar@quality-lab.ru>
2017-02-14 12:26:41 +0200

    Сравнение упорядочных списков для создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -18,8 +18,9 @@ public class GroupData {
   }
 
 
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
 
@@ -39,16 +40,19 @@ public class GroupData {
     return footer;
   }
 
+
+
+  public int getId() {
+    return id;
+  }
+
+
   @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  public int getId() {
-    return id;
   }
 
   @Override
@@ -58,16 +62,12 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
     return name != null ? name.equals ( groupData.name ) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode () : 0);
-    return result;
+    return name != null ? name.hashCode () : 0;
   }
-
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -21,9 +21,11 @@ public class GroupCreationTests extends TestBase {
     List <GroupData> after = app.getGroupHelper ().getGroupList ();
     Assert.assertEquals ( after.size (), before.size () + 1 );
 
-    group.setId ( after.stream ().max ( Comparator.comparingInt ( GroupData::getId ) ).get ().getId () );
     before.add ( group );
-    Assert.assertEquals ( new HashSet <Object> ( before ), new HashSet <Object> ( after ) );
+    Comparator <? super GroupData> byId = Comparator.comparingInt ( GroupData::getId );
+    before.sort ( byId );
+    after.sort ( byId );
+    Assert.assertEquals ( before, after );
 
   }
 
