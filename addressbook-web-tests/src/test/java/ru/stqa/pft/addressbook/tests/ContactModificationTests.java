17d810e13181c7fee28320a6082504eb1e877c49
Ivan <bondar@quality-lab.ru>
2017-02-14 12:29:20 +0200

    Сравнение упорядоченных списков для модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -39,7 +39,11 @@ public class ContactModificationTests extends TestBase {
 
     before.remove ( before.size () - 1 );
     before.add ( contact );
-    Assert.assertEquals(new HashSet<Object> ( before ), new HashSet<Object> ( after ));
+    Comparator <? super ContactData> byId = Comparator.comparingInt ( ContactData::getId );
+    before.sort ( byId );
+    after.sort ( byId );
+    Assert.assertEquals(before, after);
+
 
 
 
