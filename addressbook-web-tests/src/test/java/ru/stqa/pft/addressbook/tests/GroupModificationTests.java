da8b81c095bde73248cf0303dee41b3337744aa4
Ivan <bondar@quality-lab.ru>
2017-02-14 12:18:03 +0200

    Сравнение упорядоченных списков при модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -31,7 +33,10 @@ public class GroupModificationTests extends TestBase {
 
     before.remove ( before.size () - 1 );
     before.add ( group );
-    Assert.assertEquals(new HashSet<Object> ( before ), new HashSet<Object> ( after ));
+    Comparator <? super GroupData> byId = Comparator.comparingInt ( GroupData::getId );
+    before.sort ( byId );
+    after.sort ( byId );
+    Assert.assertEquals(before, after);
 
 
 
