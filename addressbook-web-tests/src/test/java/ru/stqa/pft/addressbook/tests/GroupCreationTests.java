9ecc3b4bc7cb04e7888e5431f56235cc669ef821
Ivan <bondar@quality-lab.ru>
2017-02-25 17:03:18 +0200

    Загрузка тестовых данных для создания группы из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,11 +18,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator <Object[]> validGroups() {
+  public Iterator <Object[]> validGroups() throws IOException {
     List <Object[]> list = new ArrayList <Object[]> ();
-    list.add ( new Object[]{new GroupData ().withName ( "test1" ).withHeader ( "header 1" ).withFooter ( "footer 1" )} );
-    list.add ( new Object[]{new GroupData ().withName ( "test2" ).withHeader ( "header 2" ).withFooter ( "footer 2" )} );
-    list.add ( new Object[]{new GroupData ().withName ( "test3" ).withHeader ( "header 3" ).withFooter ( "footer 3" )} );
+    BufferedReader resder = new BufferedReader ( new FileReader  (new File ("src/test/resources/groups.csv") ) );
+    String line = resder.readLine ();
+    while (line != null) {
+      String[] split = line.split ( ";" );
+      list.add ( new Object[] {new GroupData( ).withName ( split[0] ).withHeader ( split[1] ).withFooter ( split[2] )});
+      line = resder.readLine ();
+    }
     return list.iterator ();
   }
 
