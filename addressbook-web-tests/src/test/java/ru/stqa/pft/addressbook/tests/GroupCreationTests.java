891e43e5f4585bfb633caf6280561d9e28fc9e82
Ivan <bondar@quality-lab.ru>
2017-02-25 18:14:53 +0200

    Файл в формате json для создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -20,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator <Object[]> validGroups() throws IOException {
+  public Iterator <Object[]> validGroupsFromXml() throws IOException {
     BufferedReader resder = new BufferedReader ( new FileReader  (new File ("src/test/resources/groups.xml") ) );
     String xml = "";
     String line = resder.readLine ();
@@ -34,9 +36,23 @@ public class GroupCreationTests extends TestBase {
     return groups.stream ().map((g) -> new Object[] {g}).collect ( Collectors.toList () ).iterator ();
 
   }
+  @DataProvider
+  public Iterator <Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader resder = new BufferedReader ( new FileReader  (new File ("src/test/resources/groups.json") ) );
+    String json = "";
+    String line = resder.readLine ();
+    while (line != null) {
+      json += line;
+      line = resder.readLine ();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson ( json, new TypeToken<List<GroupData>> (){}.getType ());
+    return groups.stream ().map((g) -> new Object[] {g}).collect ( Collectors.toList () ).iterator ();
+
+  }
 
 
-  @Test(dataProvider = "validGroups")
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo ().groupPage ();
     Groups before = (Groups) app.group ().all ();
