034e5379b19fb2ca61c19fe63b494be591ab89f5
Ivan <bondar@quality-lab.ru>
2017-02-26 17:57:49 +0200

    Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,13 +6,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Administrator on 28.01.2017.
  */
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private ContactHelper contactHelper;
@@ -21,11 +27,15 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser)  {
     this.browser = browser;
+    properties = new Properties (  );
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty ( "target", "local" );
+    properties.load ( new FileReader ( new File ( String.format ( "src/test/resources/%s.properties", target ) ) ) );
+
     if (Objects.equals ( browser, BrowserType.FIREFOX )) {
       wd = new FirefoxDriver ();
     } else if (Objects.equals ( browser, BrowserType.CHROME )) {
@@ -35,12 +45,12 @@ public class ApplicationManager {
     }
 
     wd.manage ().timeouts ().implicitlyWait ( 0, TimeUnit.SECONDS );
-    wd.get ( "http://localhost/addressbook/" );
+    wd.get (properties.getProperty ( "web.baseUrl" ));
     groupHelper = new GroupHelper ( wd );
     navigationHelper = new NavigationHelper ( wd );
     sessionHelper = new SessionHelper ( wd );
     contactHelper = new ContactHelper ( wd );
-    sessionHelper.login ( "admin", "secret" );
+    sessionHelper.login (properties.getProperty ( "web.AdminLogin" ),properties.getProperty ( "web.AdminPassword" )  );
   }
 
 
