d2bad16420106a29c06d0778e9f0dfe0fe081e53
Ivan <bondar@quality-lab.ru>
2017-03-04 22:26:56 +0200

    Сравнение данных из БД при удалении групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -13,18 +13,19 @@ public class GroupDeletionTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo ().groupPage ();
-    if (app.group ().all ().size () == 0) {
+    if (app.db ().groups ().size () ==0) {
+      app.goTo ().groupPage ();
       app.group ().create ( new GroupData ().withName ( "test1" ) );
     }
   }
 
   @Test
   public void testGroupDeletion() {
-    Groups before = app.group ().all ();
+    Groups before = app.db ().groups ();
     GroupData deletedGroup = before.iterator ().next ();
     app.group ().delete ( deletedGroup );
     assertThat ( app.group ().count (), equalTo ( before.size () - 1 ) );
-    Groups after = app.group ().all ();
+    Groups after = app.db ().groups ();
     assertThat ( after, equalTo ( before.without ( deletedGroup ) ) );
 
 
