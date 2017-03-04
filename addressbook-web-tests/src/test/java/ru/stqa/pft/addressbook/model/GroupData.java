965abfc555845de660efe6a5ac501df654827f6f
Ivan <bondar@quality-lab.ru>
2017-03-04 22:19:31 +0200

    Сравнение данных из БД при модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -72,24 +72,6 @@ public class GroupData {
   }
 
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass () != o.getClass ()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals ( groupData.name ) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode () : 0);
-    return result;
-  }
-
   public int getId() {
     return id;
   }
@@ -103,4 +85,25 @@ public class GroupData {
             '}';
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass () != o.getClass ()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    if (name != null ? !name.equals ( groupData.name ) : groupData.name != null) return false;
+    if (header != null ? !header.equals ( groupData.header ) : groupData.header != null) return false;
+    return footer != null ? footer.equals ( groupData.footer ) : groupData.footer == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode () : 0);
+    result = 31 * result + (header != null ? header.hashCode () : 0);
+    result = 31 * result + (footer != null ? footer.hashCode () : 0);
+    return result;
+  }
 }
