242fdb3b780e036768154c1391ccf9cf276cbfc5
Ivan <bondar@quality-lab.ru>
2017-03-12 01:12:10 +0200

    В тест RegistrationTests  добавлено отключение капчи

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
 
   private String browser;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
 
   public ApplicationManager(String browser)  {
@@ -58,6 +59,14 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper ( this );
     }
     return registrationHelper;
+
+  }
+
+  public  FtpHelper ftp () {
+    if (ftp == null) {
+      ftp = new FtpHelper ( this );
+    }
+    return ftp;
 
   }
 
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = '7/j/9tbkUqYqACKam1IugrI7UIW/pPgP+WF/vXXtuyk=';
+
+$g_signup_use_captcha = OFF;
